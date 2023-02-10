a72f85f1a3862ef08d1e63384f0faa048507c441
yoonjong.park <yoonjongpark@gmail.com>
2023-02-10 09:06:40 +0900

    ADD nex button & animate for moving Box

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import styled from "styled-components";
 
@@ -8,45 +8,69 @@ const Wrapper = styled(motion.div)`
   display: flex;
   justify-content: center;
   align-items: center;
+  flex-direction: column;
   background: linear-gradient(135deg, #006eff, #b3ff00);
 `;
 
 const Box = styled(motion.div)`
   width: 400px;
   height: 200px;
-  position: absolute;
   border-radius: 40px;
+  display: flex;
+  position: absolute;
   top: 100px;
+  justify-content: center;
+  align-items: center;
   margin-bottom: 20px;
   background-color: white;
 `;
 
 const BoxVariants = {
-  initial: {
+  invisible: {
+    x: 500,
     opacity: 0,
     scale: 0,
   },
-  visible: { opacity: 1, scale: 1, rotateZ: 360 },
-  leaving: { opacity: 0, scale: 0, y: 20 },
+  visible: {
+    x: 0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration: 0.5,
+    },
+  },
+  exit: { x: -500, opacity: 0, scale: 0, transition: { duration: 0.5 } },
 };
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const toggleShowing = () => setShowing(!showing);
+  const [visible, setVisible] = useState(1);
+  const prevPlease = useCallback(() => {
+    setVisible(prev => (prev === 1 ? 1 : prev - 1));
+  }, []);
+
+  const nextPlease = useCallback(() => {
+    setVisible(prev => (prev === 10 ? 10 : prev + 1));
+  }, []);
 
   return (
     <Wrapper>
       <AnimatePresence>
-        {showing && (
-          <Box
-            variants={BoxVariants}
-            initial="initial"
-            animate="visible"
-            exit="leaving"
-          />
+        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map(
+          i =>
+            i === visible && (
+              <Box
+                key={i}
+                variants={BoxVariants}
+                initial="invisible"
+                animate="visible"
+                exit="exit">
+                {i}
+              </Box>
+            )
         )}
       </AnimatePresence>
-      <button onClick={toggleShowing}>click</button>
+      <button onClick={prevPlease}>prev</button>
+      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
