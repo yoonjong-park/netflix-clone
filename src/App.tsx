7049d29285e0fcae0abcfcb6f60190adbca3c41d
yoonjong.park <yoonjongpark@gmail.com>
2023-02-12 18:36:22 +0900

    ADD overlay & move Div  when Clicked

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback, useState } from "react";
+import React, { useState } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import styled from "styled-components";
 
@@ -6,76 +6,68 @@ const Wrapper = styled(motion.div)`
   height: 100vh;
   width: 100vw;
   display: flex;
-  justify-content: center;
+  justify-content: space-around;
   align-items: center;
-  flex-direction: column;
   background: linear-gradient(135deg, #006eff, #b3ff00);
 `;
 
-const Box = styled(motion.div)`
-  width: 400px;
-  height: 200px;
-  border-radius: 40px;
-  display: flex;
-  position: absolute;
-  top: 100px;
-  justify-content: center;
-  align-items: center;
-  margin-bottom: 20px;
-  background-color: white;
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap: 10px;
+  div:first-child,
+  div:last-child {
+    grid-column: span 2;
+  }
 `;
 
-const box = {
-  entry: (isBack: boolean) => ({
-    x: isBack ? -500 : 500,
-    opacity: 0,
-    scale: 0,
-  }),
-  center: {
-    x: 0,
-    opacity: 1,
-    scale: 1,
-    transition: {
-      duration: 0.5,
-    },
-  },
-  exit: (isBack: boolean) => ({
-    x: isBack ? 500 : -500,
-    opacity: 0,
-    scale: 0,
-    transition: { duration: 0.5 },
-  }),
+const Box = styled(motion.div)`
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 40px;
+  height: 200px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5);
+  position: absolute;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const overlay = {
+  start: { backgroundColor: "rgba(0, 0, 0, 0)" },
+  animate: { backgroundColor: "rgba(0, 0, 0, 0.5)" },
+  end: { backgroundColor: "rgba(0, 0, 0, 0)" },
 };
 
 function App() {
-  const [visible, setVisible] = useState(1);
-  const [back, setBack] = useState(false);
-
-  const prevPlease = async () => {
-    await setBack(false);
-    setVisible(prev => (prev === 1 ? 1 : prev - 1));
-  };
-
-  const nextPlease = async () => {
-    await setBack(true);
-    setVisible(prev => (prev === 10 ? 10 : prev + 1));
-  };
+  const [clicked, setClicked] = useState(false);
+  const toggle = () => setClicked(prev => !prev);
 
   return (
-    <Wrapper>
-      <AnimatePresence mode="wait">
-        <Box
-          custom={back}
-          key={visible}
-          variants={box}
-          initial="entry"
-          animate="center"
-          exit="exit">
-          {visible}
-        </Box>
+    <Wrapper onClick={toggle}>
+      <Grid>
+        <Box layoutId="hello" />
+        <Box />
+        <Box />
+        <Box />
+      </Grid>
+      <AnimatePresence>
+        {clicked ? (
+          <Overlay
+            variants={overlay}
+            initial="start"
+            animate="animate"
+            exit="end">
+            <Box layoutId="hello" style={{ width: 400, height: 200 }} />
+          </Overlay>
+        ) : null}
       </AnimatePresence>
-      <button onClick={prevPlease}>prev</button>
-      <button onClick={nextPlease}>next</button>
     </Wrapper>
   );
 }
